9101fd0e777aa592e0c18da7a2e3cc843d7b1354
edwin rivera <waehake@gmail.com>
2018-01-17 19:52:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 
 function forLoop(arr) {
   for (var i = 0; i < arr.length; i++) {
-    console.log(`I am ${i} strange loop ${i === 0 ? '' : 's'}.`);
+    arr.push(`I am ${i} strange loop ${i === 0 ? '' : 's'}.`);
   }
 }
